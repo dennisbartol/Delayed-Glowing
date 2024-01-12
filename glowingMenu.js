c48d97df1706e94f8e25321129c4f25605cf7f3d
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-12 18:59:36 +0100

    Update glowingMenu.js

--- glowingMenu.js
+++ glowingMenu.js
@@ -1,1 +1,8 @@
 // GlowingMenu
+
+
+
+let cursor = document.querySelector('#cursor'); 
+document.addEventListener('mousemove', (e) => {
+  cursor.style.top = e.pageY + 'px' ; 
+  cursor.style.left = e.pageX + 'px' ; 
