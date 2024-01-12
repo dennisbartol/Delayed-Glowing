bb587aa705072af208275e1aedf1fce60c583265
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-12 19:23:04 +0100

    Update glowingMenu.js

--- glowingMenu.js
+++ glowingMenu.js
@@ -1,6 +1,9 @@
 // GlowingMenu
-
-
+let text = document.querySelectorAll('ul li a').forEach
+  (text => {
+    text.innerHTML = text.innerText.split('').map((letters, 
+    i) => `<span style="transition-delay:${i*85}ms;">${letters}</span>`).join('');
+  })
 
 let cursor = document.querySelector('#cursor'); 
 document.addEventListener('mousemove', (event) => {
